c92d41287aebf5540b4d3cd200d694c808db4646
RajenderSinghB <prince00430@gmail.com>
2020-11-24 12:29:28 +0530

    additional component for listIteam added

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, TouchableHighlight } from 'react-native';
 import AppText from './AppText';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import colors from '../config/colors';
 
-function ListItem({ title, subTitle, image }) {
+function ListItem({
+    title,
+    subTitle,
+    image,
+    ImageComponent,
+    onPress,
+    renderRightActions,
+}) {
     return (
-        <View style={styles.container}>
-            <Image style={styles.image} source={image} />
-            <View>
-                <AppText style={styles.title}>{title}</AppText>
-                <AppText style={styles.subTitle}>{subTitle}</AppText>
-            </View>
-        </View>
+        <Swipeable renderRightActions={renderRightActions}>
+            <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+                <View style={styles.container}>
+                    {ImageComponent}
+                    {image && <Image style={styles.image} source={image} />}
+                    <View style={styles.detailsContainer}>
+                        <AppText style={styles.title}>{title}</AppText>
+                        {subTitle && (
+                            <AppText style={styles.subTitle}>
+                                {subTitle}
+                            </AppText>
+                        )}
+                    </View>
+                </View>
+            </TouchableHighlight>
+        </Swipeable>
     );
 }
 
@@ -25,13 +42,16 @@ const styles = StyleSheet.create({
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight: 10,
     },
     subTitle: {
         color: colors.medium,
     },
     title: {
         fontWeight: '500',
+    },
+    detailsContainer: {
+        marginLeft: 10,
+        justifyContent: 'center',
     },
 });
 
